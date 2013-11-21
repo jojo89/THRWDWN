e5193de97866deed791509a8f52b137c3a4f64bc
jojo89 <burtoftallbiz@gmail.com>
2013-11-21 12:02:08 -0600

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Throwdown::Application.config.secret_key_base = '0f41c9aea3a566e84c0bea479a1695e8dbca04b86c19ae83fc31ddfa4e046fdf3951c30e949dea251241d1829f7cfd3363fc69a875ff4191ef4ad2e0029bf6c4'
