0453a7f7ad074288bfd8e7748ec8d7e3c63bca09
jojo89 <burtoftallbiz@gmail.com>
2013-11-26 14:01:55 -0600

    scores are being saved

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -2,12 +2,13 @@ class ScoresController < ApplicationController
 
   def new
     @game = Game.find(params[:game_id])	
-    @score1 = Score.new 
-    @score2 = Score.new
   end
 
   def create
-  	
+  	@score1 = Score.create(game_id:params[:game_id], name:params[:name1])
+  	@score2 = Score.create(game_id:params[:game_id], name:params[:name2])
+  	p @score1
+  	p @score2
   end
 
 
