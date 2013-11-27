dd03e2e77f7abb1f2cf12c8cf80cdc16cc134019
jojo89 <burtoftallbiz@gmail.com>
2013-11-27 11:47:24 -0600

    grabbing the action of a form works

--- app/assets/javascripts/game_page.js
+++ app/assets/javascripts/game_page.js
@@ -1,6 +1,12 @@
 $( document ).ready(function() {
     $('.nonselected').on('click',function(e){
     	$(this).removeClass('nonselected')
-    	$(this).addClass('selected')
+    	$(this).addClass('selected')	
     })
+    $('.edit_roll').on('click',function(e){
+
+      e.preventDefault();
+      var url = $(this).attr("action")
+      console.log(url)
+    });
 });
--- app/controllers/rolls_controller.rb
+++ app/controllers/rolls_controller.rb
@@ -12,12 +12,17 @@ class RollsController < ApplicationController
 
   def show
   	p params
+  	@game = Game.find(params[:game_id])
   	@roll = Roll.find(params[:id])
+  	@score = Score.find(params[:score_id])
   	@dice =[]
   	@roll.dice_left.times do
   	  	@dice << rand(1..6)
   	end
   end	
+
+
+
 
 
 end	
