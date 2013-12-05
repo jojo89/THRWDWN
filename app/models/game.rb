9a9010f3831c5c63e207d3514b44dd7f576982b7
jojo89 <burtoftallbiz@gmail.com>
2013-12-05 16:43:08 -0600

    started the winner method

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,16 @@
 class Game < ActiveRecord::Base
 	has_many :scores
 	has_many :rolls, through: :scores
+
+
+    def winner
+      array = []
+      self.scores.each do |s|
+        if s.points > array.first.points
+          array << s 
+        end	
+        
+      end	
+    end	
+
 end
